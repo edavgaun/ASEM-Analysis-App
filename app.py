d28dbe5352ea7f73e15257f3aa5afc644199620d
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-04-28 19:50:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import requests
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from utils.preprocessing import load_data_full, load_df_long, load_bow_dfs, load_dfs
+from utils.text_processing import load_stopwords, load_spacy_model
+from utils.constants import YEAR_RANGE, COLOR_RED, GRID_STYLE
 from Charts.bubble_chart import plot_bubble_chart
 from Charts.radar_chart import compare_radar_streamlit
 
@@ -23,7 +26,14 @@ def load_stopwords():
     return stopwords
 
 own_stopwords = load_stopwords()
+nlp = load_spacy_model()  # Only if needed
 st.success(f"The custom stopword dictionary contains {len(own_stopwords)} terms.")
+
+# Load all data
+data_full = load_data_full()
+df_long = load_df_long()
+bow_dfs = load_bow_dfs()
+dfs = load_dfs()
 
 # Create bubble chart
 top_words = st.multiselect("Select words to display:", options=sorted(data_full.index), default=[])
