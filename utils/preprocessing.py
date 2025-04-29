0a727276cda2ebc8dbb5dbc9fa6e9c76f5f6d8d8
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-04-28 21:12:33 -0600

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -2,23 +2,16 @@ import pandas as pd
 import os
 
 def load_data_full():
-    url = "https://raw.githubusercontent.com/edavgaun/ASEM-Analysis-App/main/Data/data_full.csv"
+    url = "Data/data_full.csv"
     return pd.read_csv(url, index_col=0)
 
 def load_df_long():
-    url = "https://raw.githubusercontent.com/edavgaun/ASEM-Analysis-App/main/Data/df_long.csv"
+    url = "Data/df_long.csv"
     return pd.read_csv(url)
-
-def load_bow_dfs():
-    bow_dfs = {}
-    for year in range(2015, 2025):
-        url = f"https://raw.githubusercontent.com/edavgaun/ASEM-Analysis-App/main/Data/bow_df_{year}.csv"
-        bow_dfs[year] = pd.read_csv(url)
-    return bow_dfs
 
 def load_dfs():
     dfs = {}
     for year in range(2015, 2025):
-        url = f"https://raw.githubusercontent.com/edavgaun/ASEM-Analysis-App/main/Data/df_{year}.csv"
+        url = f"Data/CP_{year}.csv"
         dfs[year] = pd.read_csv(url)
     return dfs
