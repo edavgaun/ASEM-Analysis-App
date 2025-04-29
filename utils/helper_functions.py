6e9b27a6604726544f9a0948c216a3c3bd348249
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-04-28 21:15:00 -0600

    Update helper_functions.py

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -2,6 +2,43 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+def load_bow_dfs():
+    bow_dfs = {}
+    for year in range(2015, 2025):
+        url = f"Data/bow_df_{year}.csv"
+        bow_dfs[year] = pd.read_csv(url)
+    return bow_dfs
+    
+def get_corpus(df, year):
+  corpus=", ".join([t.lower() if type(t)!=float else "" for t in df.Title.values])
+  if year!=2015:
+    corpus+=", ".join([t.lower() if type(t)!=float else "" for t in df.KeyWords.values])
+  corpus+=", ".join([t.lower() if type(t)!=float else "" for t in df.Abstract.values])
+  corpus=", ".join([c.replace(" ", "-").replace(";", ",").replace(".", ",").replace("-,", "") for c in corpus.split(", ")])
+  corpus=corpus.replace("4,0", "4.0").replace("5,0", "5.0")
+  return corpus
+
+def get_tokens(corpus, nlp=nlp):
+  concepts=[t.replace("--", "-").replace("-", " ") for t in set(corpus.split(", ")) if len(t)>=2]
+  doc = nlp(", ".join(concepts))
+  return doc
+
+def get_bow(tokens):
+  bow_kw={}
+  for token in tokens:
+    if (not token.is_stop) and (not token.is_punct) and (not token.is_digit) \
+        and (len(token)>=2):
+      try:
+        bow_kw[token.lemma_]+=1
+      except:
+        bow_kw[token.lemma_]=1
+  return bow_kw
+
+def get_bow_df(bow):
+  df_kw=pd.DataFrame({"Word":bow.keys(), "frq":bow.values()}).sort_values("frq", ascending=False
+                                                                                ).reset_index(drop=True)
+  return df_kw
+
 def get_topN_word_bow_df(N, bow_df):
     """Return the top N most frequent words from a bag-of-words DataFrame."""
     return bow_df.sort_values("frq", ascending=False).head(N)["Word"].tolist()
