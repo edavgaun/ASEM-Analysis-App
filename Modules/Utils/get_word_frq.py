8670bd01e2245baa716cb45c4a85345dcf55d9e5
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 16:45:18 -0500

    Update get_word_frq.py

--- Modules/Utils/get_word_frq.py
+++ Modules/Utils/get_word_frq.py
@@ -2,12 +2,6 @@ from collections import Counter
 
 def get_word_frq(tokens):
     """
-    Returns word frequency dictionary from a spaCy Doc object.
-    Filters out stop words and non-alphabetic tokens.
+    Returns word frequency dictionary from a list of tokens.
     """
-    words = [
-        token.text.lower()
-        for token in tokens
-        if token.is_alpha and not token.is_stop
-    ]
-    return Counter(words)
+    return Counter(tokens)
