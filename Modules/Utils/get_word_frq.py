fe37711ff37b4647ab252489d716a0dcd6d549a6
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 18:02:14 -0500

    Update get_word_frq.py

--- Modules/Utils/get_word_frq.py
+++ Modules/Utils/get_word_frq.py
@@ -1,7 +1,3 @@
-from collections import Counter
-
-def get_word_frq(tokens):
-    """
-    Returns word frequency dictionary from a list of tokens.
-    """
-    return Counter(tokens)
+def get_word_frq(bow_df, Top_KW):
+    filtered_df = bow_df[bow_df["Word"].isin(Top_KW)]
+    return filtered_df.set_index("Word")["frq"].to_dict()
