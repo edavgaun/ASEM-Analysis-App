a0ca3511fd14be8e7429ee2c93fde684b58d26d2
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 17:58:23 -0500

    Update get_bow.py

--- Modules/Utils/get_bow.py
+++ Modules/Utils/get_bow.py
@@ -1,10 +1,15 @@
 def get_bow(tokens):
-  bow_kw={}
-  for token in tokens:
-    if (not token.is_stop) and (not token.is_punct) and (not token.is_digit) \
-        and (len(token)>=2):
-      try:
-        bow_kw[token.lemma_]+=1
-      except:
-        bow_kw[token.lemma_]=1
-  return bow_kw
+    bow_kw = {}
+    for token in tokens:
+        if (
+            not token.is_stop
+            and not token.is_punct
+            and not token.is_digit
+            and len(token) >= 2
+        ):
+            lemma = token.lemma_
+            if lemma in bow_kw:
+                bow_kw[lemma] += 1
+            else:
+                bow_kw[lemma] = 1
+    return bow_kw
