3bbf337aa7ae02ac6adc74ee5dc900a19c721831
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 18:56:26 -0500

    Update get_wide_df.py

--- Modules/Utils/get_wide_df.py
+++ Modules/Utils/get_wide_df.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import streamlit as st
 
+@st.cache_data
 def get_wide_df():
     url = "https://raw.githubusercontent.com/edavgaun/ASEM-Analysis-App/main/Data/data_full.csv"
     df = pd.read_csv(url)
