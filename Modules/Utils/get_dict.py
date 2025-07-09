23c74b1a5bb4aebcd48b681805a14c7450dd6da8
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 18:00:47 -0500

    Update get_dict.py

--- Modules/Utils/get_dict.py
+++ Modules/Utils/get_dict.py
@@ -1,9 +1,8 @@
 import requests
 
 def get_dict():
-  url="https://raw.githubusercontent.com/edavgaun/ASEM-Analysis-App/main/Data/own_stopwords.txt"
-  response = requests.get(url)
-  content = response.text
-  content = content.strip().replace("\n", ", ").split(", ")
-  content.sort()
-  return content
+    url = "https://raw.githubusercontent.com/edavgaun/ASEM-Analysis-App/main/Data/own_stopwords.txt"
+    response = requests.get(url)
+    content = response.text.strip().replace("\n", ", ").split(", ")
+    content = sorted([word for word in content if word])
+    return content
