bfadb12751254a51682e145fc67256aea1ee0fe3
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 18:29:47 -0500

    Update get_tokens.py

--- Modules/Utils/get_tokens.py
+++ Modules/Utils/get_tokens.py
@@ -1,7 +1,5 @@
-import streamlit as st
 import re
 
 def get_tokens(corpus):
     tokens = re.findall(r'\b[a-zA-Z]{2,}\b', corpus.lower())
-    st.write(f"Token sample: {tokens[:10]}")
     return tokens
