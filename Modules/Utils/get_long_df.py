6f96f4f57f764fc382d346b59663425f1e93e141
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 18:56:10 -0500

    Update get_long_df.py

--- Modules/Utils/get_long_df.py
+++ Modules/Utils/get_long_df.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import streamlit as st
 
+@st.cache_data
 def get_long_df():
     url = "https://raw.githubusercontent.com/edavgaun/ASEM-Analysis-App/main/Data/df_long.csv"
     df = pd.read_csv(url, index_col="Unnamed: 0")
