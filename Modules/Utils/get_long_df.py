d44a7f728b3a80dffa0ceca15aed71c6345fe8dd
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 18:49:44 -0500

    Update get_long_df.py

--- Modules/Utils/get_long_df.py
+++ Modules/Utils/get_long_df.py
@@ -1,4 +1,6 @@
+import pandas as pd
+
 def get_long_df():
-  url = "https://raw.githubusercontent.com/edavgaun/ASEM-Analysis-App/refs/heads/main/Data/df_long.csv"
-  df = pd.read_csv(url, index_col="Unnamed: 0")
-  return df
+    url = "https://raw.githubusercontent.com/edavgaun/ASEM-Analysis-App/main/Data/df_long.csv"
+    df = pd.read_csv(url, index_col="Unnamed: 0")
+    return df
