8aad12e1267100cc5d95d2b2df930f3dd2806aa2
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 18:00:05 -0500

    Update get_bow_df.py

--- Modules/Utils/get_bow_df.py
+++ Modules/Utils/get_bow_df.py
@@ -1,4 +1,6 @@
+import pandas as pd
+
 def get_bow_df(bow):
-  df_kw=pd.DataFrame({"Word":bow.keys(), "frq":bow.values()}).sort_values("frq", ascending=False
-                                                                                ).reset_index(drop=True)
-  return df_kw
+    df_kw = pd.DataFrame({"Word": list(bow.keys()), "frq": list(bow.values())})
+    df_kw = df_kw.sort_values("frq", ascending=False).reset_index(drop=True)
+    return df_kw
