b070a6099649627ab3be763be156b7f9996416b2
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 18:01:30 -0500

    Update get_topN_word_bow_df.py

--- Modules/Utils/get_topN_word_bow_df.py
+++ Modules/Utils/get_topN_word_bow_df.py
@@ -1,2 +1,6 @@
-def get_topN_word_bow_df(num_word, bow_df, own_stopwords=own_stopwords):
-  return bow_df[~bow_df.Word.isin(own_stopwords)].head(num_word).Word.values
+from Modules.Utils.get_dict import get_dict
+
+def get_topN_word_bow_df(num_word, bow_df, own_stopwords=None):
+    if own_stopwords is None:
+        own_stopwords = get_dict()
+    return bow_df[~bow_df["Word"].isin(own_stopwords)].head(num_word)["Word"].values
