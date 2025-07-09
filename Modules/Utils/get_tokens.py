d7b7488694ac61efeb59eff132c051d8dff5399e
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 15:34:38 -0500

    Update get_tokens.py

--- Modules/Utils/get_tokens.py
+++ Modules/Utils/get_tokens.py
@@ -1,4 +1,14 @@
-def get_tokens(corpus, nlp):
-  concepts=[t.replace("--", "-").replace("-", " ") for t in set(corpus.split(", ")) if len(t)>=2]
-  doc = nlp(", ".join(concepts))
-  return doc
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def get_tokens(corpus, nlp_model=nlp):
+    """
+    Tokenizes the cleaned corpus using spaCy.
+    """
+    concepts = [
+        t.replace("--", "-").replace("-", " ")
+        for t in set(corpus.split(", ")) if len(t) >= 2
+    ]
+    doc = nlp_model(", ".join(concepts))
+    return doc
