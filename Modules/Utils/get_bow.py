b25416b6890e053451959c20a4384fe826195d7b
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 18:30:22 -0500

    Update get_bow.py

--- Modules/Utils/get_bow.py
+++ Modules/Utils/get_bow.py
@@ -1,14 +1,10 @@
-import re
-
 def get_bow(tokens):
     bow_kw = {}
     for token in tokens:
         # Filter out tokens with digits, punctuation, or length < 2
         if len(token) < 2:
             continue
-        if re.search(r"\d", token):
-            continue
-        if re.search(r"[\W_]", token):  # non-word characters or underscore
+        if not token.isalpha():
             continue
         bow_kw[token] = bow_kw.get(token, 0) + 1
     return bow_kw
