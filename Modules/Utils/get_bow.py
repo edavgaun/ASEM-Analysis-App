aa494cfd3b455e349ec3c3dc522860da65060883
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-09 19:54:35 -0500

    Update get_bow.py

--- Modules/Utils/get_bow.py
+++ Modules/Utils/get_bow.py
@@ -1,13 +1,15 @@
-from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
-stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
 
 def get_bow(tokens):
     bow_kw = {}
     for token in tokens:
-        token = token.lower()
-        if len(token) < 2 or not token.isalpha():
+        if len(token) < 2:
             continue
-        stemmed = stemmer.stem(token)
-        bow_kw[stemmed] = bow_kw.get(stemmed, 0) + 1
+        if not token.isalpha():
+            continue
+
+        lemma = lemmatizer.lemmatize(token)
+        bow_kw[lemma] = bow_kw.get(lemma, 0) + 1
     return bow_kw
