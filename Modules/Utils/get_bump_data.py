b321160ad2b60ea8e118a612028ff8cdeeedd629
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-12 13:15:29 -0500

    Update get_bump_data.py

--- Modules/Utils/get_bump_data.py
+++ Modules/Utils/get_bump_data.py
@@ -6,15 +6,13 @@ nltk.download('stopwords', quiet=True)
 from Modules.Utils.get_dict import get_dict
 import streamlit as st
 
-def get_bump_data(bow_dfs, k=12, custom_stopwords=None):
-    if custom_stopwords is None:
-        custom_stopwords = []
-    else:
-        custom_stopwords=get_dict()
+def get_bump_data(bow_dfs, k=12, custom_stopwords=True):
+    from nltk.corpus import stopwords
 
-    filter_words = ["  ", "review"] + custom_stopwords
-    streamlit_words = ['based', 'data', 'ha', 'keywords', 'used', 'wa']
-    filter_words += streamlit_words
+    filter_words = ["  ", "review"] + ['based', 'data', 'ha', 'keywords', 'used', 'wa']
+
+    if custom_stopwords:
+        filter_words += get_dict()
 
     if 'system' in filter_words:
         filter_words.remove('system')
@@ -27,7 +25,6 @@ def get_bump_data(bow_dfs, k=12, custom_stopwords=None):
         df = bow_dfs[year]
         filtered = df[~df.Word.isin(stopwords.words("english"))].Word.reset_index()
         filtered = filtered[~filtered.Word.isin(filter_words)].head(k).Word
-        st.write(filtered.Word)
         topW = [f"({i}), {str(t)}" for i, t in zip(filtered.index.tolist(), filtered.values.tolist())]
         table.loc[row, "Year"] = year
         for col, word in enumerate(topW):
@@ -35,18 +32,17 @@ def get_bump_data(bow_dfs, k=12, custom_stopwords=None):
 
     table["Year"] = table["Year"].astype(int)
 
-    bump_words={z:[] for z in set([w.split(", ")[1] for y in table.iloc[:, 1:].values for w in y])}
+    bump_words = {z: [] for z in set([w.split(", ")[1] for y in table.iloc[:, 1:].values for w in y])}
     for _, row in table.iterrows():
         words_present = {}
         for cell in row[1:]:  # skip Year column
-                rank_str, word = cell.split(", ")
-                rank = int(rank_str.strip("()"))
-                words_present[word] = rank
-        
-        # For each keyword in bump_words, append rank or np.nan for this year
+            rank_str, word = cell.split(", ")
+            rank = int(rank_str.strip("()"))
+            words_present[word] = rank
+
         for word in bump_words:
             if word in words_present:
-                bump_words[word].append(words_present[word]+1)
+                bump_words[word].append(words_present[word] + 1)
             else:
                 bump_words[word].append(np.nan)
 
