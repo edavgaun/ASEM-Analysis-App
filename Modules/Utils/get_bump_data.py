6ef997269bdc493c45f389adfe645fb7a0caf3e8
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-11 20:54:19 -0500

    Update get_bump_data.py

--- Modules/Utils/get_bump_data.py
+++ Modules/Utils/get_bump_data.py
@@ -20,7 +20,7 @@ def get_bump_data(bow_dfs, k=12):
         year = 2015 + row
         df = bow_dfs[year]
         filtered = df[~df.Word.isin(stopwords.words("english"))].head(k).Word.reset_index()
-        filtered = filtered[~filtered.Word.isin(filter_words)].head(k).Word
+        #filtered = filtered[~filtered.Word.isin(filter_words)].head(k).Word
         st.write(filtered)
         topW = [f"({i}), {str(t)}" for i, t in zip(filtered.index.tolist(), filtered.values.tolist())]
         table.loc[row, "Year"] = year
